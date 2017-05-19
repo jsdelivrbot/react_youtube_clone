0837a30e1ed671ed34d669ccee6d82f85117b449
junhua <junhua.viva@gmail.com>
2017-05-19 16:25:42 +0800

    added search function, all done

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,17 +1,29 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      term: ''
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        };
     };
-  }
 
-  render = () => <div>
-    < input value={this.state.term} onChange= { event => this.setState({term: event.target.value}) }/>
-  </div>
+    onInputChange(term){
+        this.setState({term});
+        this.props.onSearchTermChange(term);
 
+    }
+    render() {
+        return (
+            <div className="search-bar">
+                <input
+                    value={this.state.term}
+                    onChange={event => this.onInputChange(event.target.value)}
+                />
+            </div>
+        );
+    };
 }
 
 export default SearchBar;
